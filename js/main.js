1847dc681f7785cf17f71d3e46137b836deb15db
Karen Lobin Perkins <karen@Akula.attlocal.net>
2020-08-01 10:52:02 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+  
+/*------Constants------*/
+
+console.log(board);
+
+/*------Variables (state)------*/
+
+// Variables might include (board/turn/winner)
+let board, turn, winner;
+
+
+
+
+/*------Cached Element References------*/
+
+// You might choose to put your game status here
+const messageEl = document.getElementById('message');
+const sq0El = document.getElementById('sq0');
+const sq1El = document.getElementById('sq1');
+const sq2El = document.getElementById('sq2');
+const sq3El = document.getElementById('sq3');
+const sq4El = document.getElementById('sq4');
+const sq5El = document.getElementById('sq5');
+const sq6El = document.getElementById('sq6');
+const sq7El = document.getElementById('sq7');
+const sq8El = document.getElementById('sq8');
+
+
+/*------Event Listeners------*/
+
+// This is where you should put the event listener
+// for a mouse-click
+
+/*------Functions------*/
+function init() {
+    
+}
+
+// Some functions you might choose to use:
+
+// Initialization function:
+// Where you set your initial state, setting up 
+// what the board will look like upon loading
+
+// On-Click function:
+// Set up what happens when one of the elements
+// is clicked
+
+
+// Check winner function:
+// Checks the current state of the board for
+// a winner and changes the state of the winner
+// variable if so
+
+
+// Render function:
+// Displays the current state of the board
+// on the page, updating the elements to reflect
+// either X or O depending on whose turn it is
