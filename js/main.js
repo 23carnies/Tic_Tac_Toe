2108cd73916975f9d0253e468217fb0ddddb9f87
Karen Lobin Perkins <karen@Akula.attlocal.net>
2020-08-01 21:00:36 -0400

    Set variables

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/*------Variables (state)------*/
+
+// Variables might include (board/turn/winner)
+//turn is to remember whose turn it is
+
+let board, turn, winner;
+//winner = [//player that won, a tie, game in play]
