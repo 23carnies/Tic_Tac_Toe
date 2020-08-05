22dd976f4e03054e795f3fcbfd706357b1c00858
Karen Lobin Perkins <karen@Akula.attlocal.net>
2020-08-04 21:27:29 -0400

    Finish isWinner, render, and handleClick funtions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 /*------Constants------*/
-const playerX = 1;
-const playerY = -1;
+const colors = {
+    'null': 'white',
+    '1': 'gold',
+    '-1': 'green'
+}
 
 /*------Variables (state)------*/
 let board = [null, null, null, null, null, null, null, null, null];
@@ -12,15 +15,6 @@ let gameStatus;
 
 /*------Cached Element References------*/
 const messageEl = document.getElementById('message');
-// const sq0El = document.getElementById('sq0');
-// const sq1El = document.getElementById('sq1');
-// const sq2El = document.getElementById('sq2');
-// const sq3El = document.getElementById('sq3');
-// const sq4El = document.getElementById('sq4');
-// const sq5El = document.getElementById('sq5');
-// const sq6El = document.getElementById('sq6');
-// const sq7El = document.getElementById('sq7');
-// const sq8El = document.getElementById('sq8');
 const resetEl = document.getElementById('reset');
 const boardSquares = document.getElementById('mainBoard');
 
@@ -28,7 +22,7 @@ const boardSquares = document.getElementById('mainBoard');
 resetEl.addEventListener('click', function(){
     init();
 });
-document.getElementById('mainBoard').addEventListener('click', handleClick());
+boardSquares.addEventListener('click', handleClick);
 
 
 
@@ -38,60 +32,58 @@ init();
 
 function init(){
     winner = false;
-    turn: 1;
-    //gamestatus.textcontent = it's x turn
+    turn = 1;
+    //player = 1;
+    gameStatus.innerText = `It's ${colors["-1"]}'s turn. `
     messageEl.innerText = 'Ready to play?!';
 }
 
 /*--------HandleClick-------*/
-function handleClick(){
+function handleClick(evt){
     //getting the index of the square
-    let squareIndex = parseint(evt.target.id.replace('sq', ''));
+    let squareIndex = parseInt(evt.target.id.replace('sq', ''));
     //see if square is empty, return if no
     for(let i=0;i<board.length;i++){
         if(board[squareIndex] !== null){
             return;
-            console.log(squareIndex);
-    }
+    } 
+    } //console.log(squareIndex);
+    render(squareIndex);
 }
+
 
 /*      RENDER FUNCTION         */
-function render() {
-    for(let i=0;i<board.length;i++){
-        if(board[squareIndex])
-    }
-}
-
-
-function changeTurn(){
-    turn *= -1;
-}
-
-
-function addLetterToSquare(){
+function render(squareIndex) {
     if (winner === null) {
         addLetter = document.getElementById(`sq${squareIndex}`);
         board[squareIndex] = turn;
         if (turn === 1) {
-            addLetter.innerHTML = 'X';
+            addLetter.innerText = "X";
         } else {
-            addLetter.innerHTML = 'O';
+            addLetter.innerText = "O";
         }
+        turn *= -1;
+        return turn;
     }
 }
+
+
 
 /*       IS THERE A WINNER?     */
 function isWinner() {
     const winCombos = [[0,4,8], [6,4,2], [0,3,6], [1,4,7], [2,5,8], [0,1,2], [3,4,5], [6,7,8]];
     for(let i=0;i<winCombos.length;i++){
-        if(Math.abs((board[winCombos[i][0]] + board[winCombos[i][1] + board[winCombos[i][2]]] === 3))){
+        if(Math.abs((board[winCombos[i][0]] + board[winCombos[i][1]] + board[winCombos[i][2]] === 3))){
             winner = turn;
-        } return winner;
+        } return winner;   
+        messageEl.innerText = `${colors["-1"]} is the winner!`
+        //add confetti                
     } 
     if(board.includes(null) === false){
         winner = 'T';
-        //its a tie do something
-    }
+        gameStatus.innerText = `It's a draw!`;
+    } 
+    
 }
 
 
