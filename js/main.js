94a933cc5f23f6c180386aed94ada8c5f45a733b
Karen Lobin Perkins <karen@Akula.attlocal.net>
2020-08-04 21:53:14 -0400

    Add confetti

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
 /*------Constants------*/
 const colors = {
-    'null': 'white',
-    '1': 'gold',
-    '-1': 'green'
+    '1': 'X',
+    '-1': 'O'
 }
 
 /*------Variables (state)------*/
 let board = [null, null, null, null, null, null, null, null, null];
-let turn = 1;   //playerX or playerO
-let winner = null; //player that won(1 or -1), a tie(T), or game in play(null)
-let gameStatus;
+let turn = 1;  
+let winner = null;
+
 
 
 
 /*------Cached Element References------*/
 const messageEl = document.getElementById('message');
 const resetEl = document.getElementById('reset');
-const boardSquares = document.getElementById('mainBoard');
+const gameStat = document.getElementById('gameStatus');
 
 /*------Event Listeners------*/
 resetEl.addEventListener('click', function(){
     init();
 });
-boardSquares.addEventListener('click', handleClick);
+document.getElementById('mainBoard').addEventListener('click', handleClick);
 
 
 
@@ -33,21 +32,19 @@ init();
 function init(){
     winner = false;
     turn = 1;
-    //player = 1;
-    gameStatus.innerText = `It's ${colors["-1"]}'s turn. `
+    gameStat.textContent = `It's ${colors["1"]}'s turn.`
     messageEl.innerText = 'Ready to play?!';
 }
 
 /*--------HandleClick-------*/
 function handleClick(evt){
-    //getting the index of the square
-    let squareIndex = parseInt(evt.target.id.replace('sq', ''));
-    //see if square is empty, return if no
+    let squareIndex = parseInt(evt.target.id.replace('sq',''));
+    console.log(squareIndex);
     for(let i=0;i<board.length;i++){
         if(board[squareIndex] !== null){
             return;
     } 
-    } //console.log(squareIndex);
+    } 
     render(squareIndex);
 }
 
@@ -58,13 +55,13 @@ function render(squareIndex) {
         addLetter = document.getElementById(`sq${squareIndex}`);
         board[squareIndex] = turn;
         if (turn === 1) {
-            addLetter.innerText = "X";
+            addLetter.textContent = 'X';
         } else {
-            addLetter.innerText = "O";
+            addLetter.textContent = 'O';
         }
-        turn *= -1;
-        return turn;
-    }
+    }    
+     turn *= -1; 
+     isWinner();
 }
 
 
@@ -75,9 +72,11 @@ function isWinner() {
     for(let i=0;i<winCombos.length;i++){
         if(Math.abs((board[winCombos[i][0]] + board[winCombos[i][1]] + board[winCombos[i][2]] === 3))){
             winner = turn;
-        } return winner;   
-        messageEl.innerText = `${colors["-1"]} is the winner!`
-        //add confetti                
+        } return winner;  
+        confetti.start(2000); 
+        messageEl.innerText = `${colors[turn]} is the winner!`
+        messageEl.className = 'animate__animated animate__bounce';
+        
     } 
     if(board.includes(null) === false){
         winner = 'T';
