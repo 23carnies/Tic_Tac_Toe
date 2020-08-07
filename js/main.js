c2be1e7c0fcd4fe566df113f62513de5069c45ab
23carnies <46233990+23carnies@users.noreply.github.com>
2020-08-06 20:13:39 -0400

    Have main functionality. Have confetti

--- js/main.js
+++ js/main.js
@@ -5,18 +5,17 @@ const colors = {
 }
 const winCombos = [[0,4,8], [6,4,2], [0,3,6], [1,4,7], [2,5,8], [0,1,2], [3,4,5], [6,7,8]];
 
-// const play = new Audio(/TicTacExtras/sounds/B5Door.wav);
-// const good = new Audio(/TicTacExtras/sounds/b5KoshGood.wav);
-// const perhaps = new Audio(/TicTacExtras/sounds/b5KoshPerhaps.wav);
-// const yes = new Audio(/TicTacExtras/sounds/b5KoshYes.wav);
-// const commence = new Audio(/TicTacExtras/sounds/Comencng.wav);
-// const online = new Audio(/TicTacExtras/sounds/Online.wav);
+ //const good = new Audio(`/sounds/b5KoshGood.wav`);
+ //const perhaps = new Audio(`/sounds/b5KoshPerhaps.wav`);
+ //const yes = new Audio(`/sounds/b5KoshYes.wav`);
+ //const commence = new Audio(`/sounds/Comencng.wav`);
+ //const online = new Audio(`/sounds/Online.wav`);
 
 /*------Variables (state)------*/
 let board = [null, null, null, null, null, null, null, null, null];
 let turn = 1;  
 let winner = null;
-let turnCount;
+let turnCount = 1;
 
 
 
@@ -28,13 +27,17 @@ const gameStat = document.getElementById('gameStatus');
 const ticTac = document.getElementsByTagName('h1');
 
 /*      EVENT LISTENERS     */
-//document.getElementById('resetEl').addEventListener('click', init);
+
 document.getElementById('mainBoard').addEventListener('click', handleClick);
+// document.getElementById('mainBoard').addEventListener('click', () => {
+//     const b5Door = new Audio(`/sounds/B5Door.wav`);
+//     b5Door.play();
+// });
 
 
 
 
-/*      FUNCTIONS       */
+/*-------FUNCTIONS--------*/
 
 init();
 
@@ -62,45 +65,50 @@ function handleClick(evt){
 
 /*      WINNER FUNCTION        */
 
-function isWinner(winCombos) {
+function isWinner() {
     for(let i=0;i<winCombos.length;i++){
         if(Math.abs((board[winCombos[i][0]] + board[winCombos[i][1]] + board[winCombos[i][2]] === 3))) {
             winner = turn;
             messageEl.textContent = `${colors[turn]} is the winner!`;
             //yes.play();
-            confetti.start(2000);
-            //messageEl.className = 'animate__animated animate__bounce';
+            confetti.start(4000);
 
-        // } else if (turnCount === 10 && winner === false){
-        //     winner = 'T';
-        //     gameStat.textContent = `It's a draw!`;
-            //perhaps.play();
-        }  
-        
-    } return winner;
+        } 
+        } return winner;
 }
 
 /*      RENDER FUNCTION         */
 
 function render(squareIndex) {
     messageEl.innerText = ' ';
+    
     if (winner === null) {
         let addLetter = document.getElementById(`sq${squareIndex}`);
         board[squareIndex] = turn;
         if (turn === 1) {
             gameStat.textContent = `It's O's turn.`;
             addLetter.textContent = 'X';
-
             //play.play();
-
+            turnCount++; 
         } else {
             addLetter.textContent = 'O';
             gameStat.textContent = `It's X's turn.`;
             //play.play();
-        }
-        // turnCount++;
-    }    
-     turn *= -1; 
-     isWinner(); 
+            turnCount++;
+        } 
+    } isWinner(); 
+    if (turnCount === 10 && winner === false){
+        winner = "T"
+        gameStat.textContent = `It's a draw!`;
+        //perhaps.play(); 
+     } 
+    //  if(winner !== null){
+    //      gameStat.textContent = '';
+    // } 
+    turn *= -1; 
+    return;
 }
+
+/*---------PLAY SOUND---------*/
+
 
